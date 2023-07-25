3d801aee93f62f081b79fbb3c6e69e7d90b2fea0
Akshat Jaiswal <akshatjaiswal.official@gmail.com>
2023-07-25 15:18:04 +0530

    Add Home Value Slider.

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,16 @@ import Navbar from "./Components/Navbar";
 import Result from "./components/Result.jsx";
 import SliderSelect from "./components/SliderSelect.jsx";
 import TenureSelect from "./components/TenureSelect.jsx";
+import { useState } from "react";
 function App() {
+  const [data, setData] = useState({
+    homeValue: 3000,
+    downPayment: 3000 * 0.2,
+    loanAmount: 3000 * 0.8,
+    loanTerm: 5,
+    interestRate: 5,
+  });
+
   return (
     <div className="App">
       <Navbar />
@@ -16,7 +25,7 @@ function App() {
           <Grid item xs={12} md={6}>
             {/* xs={12} means that grid will now take the entire screen on extra small screen and
             md ={6} means that it will take half of the width on medium and bigger screens*/}
-            <SliderSelect />
+            <SliderSelect data={data} setData={setData} />
             <TenureSelect />
           </Grid>
           <Grid item xs={12} md={6}>
--- src/components/SliderSelect.jsx
+++ src/components/SliderSelect.jsx
@@ -1,5 +1,30 @@
-function SliderSelect() {
-  return <div>Slider Select</div>;
+import SliderComponent from "./common/SliderComponent";
+
+// eslint-disable-next-line react/prop-types
+function SliderSelect({ data, setData }) {
+  const bankLimit = 10000;
+  return (
+    <div>
+      <SliderComponent
+        onChange={(e, value) => {
+          setData({
+            ...data,
+            homeValue: value.toFixed(0),
+            downPayment: (0.2 * value).toFixed(0),
+            loanAmount: (0.8 * value).toFixed(0),
+          });
+        }}
+        defaultValue={data.homeValue}
+        min={1000}
+        max={bankLimit}
+        steps={100}
+        unit={"₹"}
+        amount={data.homeValue}
+        label={"Home Value"}
+        value={data.homeValue}
+      ></SliderComponent>
+    </div>
+  );
 }
 
 export default SliderSelect;
